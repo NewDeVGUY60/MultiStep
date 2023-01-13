60c91aecd1c73d4355f8b38a4b7b10eb2de79276
NewDeVGUY60 <[guynicolas60@gmail.com](mailto:guynicolas60@gmail.com)>
2023-01-13 13:04:56 +0100

    rooting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserInformationComponent } from './user-information/user-information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component : UserInformationComponent },
+  {path:"user-information" , component : UserInformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
